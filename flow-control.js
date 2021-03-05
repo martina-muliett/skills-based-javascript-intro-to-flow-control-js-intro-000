c99cef60231ce9761767ba9b93462904d204f530
Martina Muliett <settembrina9@gmail.com>
2021-03-05 15:26:29 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,6 +4,7 @@ if (age >=13 && age <=19) {return "You are a teenager!"}
 
 function teenager(age) {
 if (age >=13 && age <=19) {return "You are a teenager!"}
+else if (age >=13 && age <=19) {return "You are not a teenager!"}
 }
 
 function ageChecker(age) {
