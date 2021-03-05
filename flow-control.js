eb6743d1ab478683d14b2aa9fa6a69b720c01ebb
Martina Muliett <settembrina9@gmail.com>
2021-03-05 15:36:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,11 +14,11 @@ else {return "You are a grownup"}
 }
 
 function ternaryTeenager(age) {
-if (age >=13 && age <=19) {return "You are a teenager!"}
+if (age >=13 && age <=19) {return "You are a teenager"}
 else {return "You are not a teenager"}
 }
 
 function switchAge(age) {
-if (age >=13 && age <=19) {return "You are a teenager!"}
+if (age >=13 && age <=19) {return "You are a teenager"}
 else {return "You have an age"}
 }
